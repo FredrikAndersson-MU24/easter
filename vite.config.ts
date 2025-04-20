bfa9a383fa36a7bd125b0752b8b2c709fe971432
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-04-20 22:54:17 +0200

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // För att ladda resurser relativt till index.html
   build: {
     outDir: "dist", // Output-dir för JavaScript
   },
